3541e963a8ef076c69d1969ddd1e694907eddb28
ofca <ofca@emve.org>
2013-06-22 02:29:52 -0700

    cleaning

--- extension/popup.js
+++
@@ -1,5 +0,0 @@
-var body = document.getElementsByTagName('body')[0];
-
-nano.bind('#saveBtn', 'click', nano.showInfo);
-
-nano.autosave('commentBoxHeight', 'removeScrolls', 'fullPostContent', 'fullCommentContent', 'defaultFont', 'fontSize', 'slimNav');
